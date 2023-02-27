6793338aca07e497e6ca2c5ad9d84c518e72a59a
jilebisundar <126415939+jilebisundar@users.noreply.github.com>
2023-02-27 15:56:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.title("SUNDAR DATA APP")
+btn_click = st.button("click me")
+
+if btn_click == True :
+    st.subheader("you clicked me i am crying :cry:")
+    st.balloons()
+    st.snow()
